2ee18d9b67478631eb9b1252d256c46986bf7862
RuslanBratus <ruslan.bratus2@gmail.com>
2022-04-14 10:38:03 +0300

    Final

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,14 @@
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    val handler = CoroutineExceptionHandler { _, exception ->
+        println("CoroutineExceptionHandler got $exception")
+    }
+    val job = GlobalScope.launch(handler) {
+        throw AssertionError()
+    }
+    val deferred = GlobalScope.async(handler) {
+        throw ArithmeticException()
+    }
+    joinAll(job, deferred)
+}
